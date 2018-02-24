46fae7aac13f6a6dc6d25c8cc75a8c8c2ef7dc12
Jayna Neilson <jaynaneilson@calvisitor-10-105-152-194.calvisitor.1918.berkeley.edu>
2018-02-24 10:10:55 -0800

    trying to fix heroku bug

--- db/schema.sql
+++ db/schema.sql
@@ -2,10 +2,4 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(140) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
-);
+her
