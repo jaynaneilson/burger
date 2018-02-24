ed4e79f78d0252ee8e650264a8d3ffdc5aaae24e
Jayna Neilson <jaynaneilson@calvisitor-10-105-152-194.calvisitor.1918.berkeley.edu>
2018-02-24 10:15:42 -0800

    heroku bug not fixed

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,8 +4,8 @@ var router = express.Router();
 
 var burgers = require("../models/burger.js");
 
-// router.get("/", function(req,res){
-// 	res.redirect("burgers")
+router.get("/", function(req,res){
+	res.redirect("burgers")
 });
 
 router.get("/burgers", function(req,res){
